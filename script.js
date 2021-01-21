c3144f32762c1e70ab0f1d39c31ac2bd4030b037
Kirill <k.semenovich@gmail.com>
2021-01-21 19:17:46 +0300

    develop branch

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
- 
+const headerMenuBtn = document.getElementById('header_menu_a');
+const promoMenuCloseBtn = document.getElementById('promo_menu_close');
+const promoMenu = document.getElementsByClassName('promo-menu')[0];
+const promoOverlay = document.getElementsByClassName('promo-overlay')[0];
+
+
+headerMenuBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    promoMenu.classList.toggle('show');
+    promoOverlay.classList.toggle('show');
+});
+
+promoMenuCloseBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    promoMenu.classList.remove('show');
+    promoOverlay.classList.remove('show');
+});
